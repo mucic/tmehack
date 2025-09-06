e634df0e3a054825c656cf43154dbbd2e2acc427
mucic <nenad.mucic@faxi.co.uk>
2025-09-06 09:34:41 +0200

    add CDN assets to distribution build version

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var sourcemaps = require('gulp-sourcemaps');
 var open = require('gulp-open');
 var fileInclude = require('gulp-file-include');
 var rename = require('gulp-rename');
+var replace = require('gulp-replace');
 
 var Paths = {
   HERE: './',
@@ -14,7 +15,8 @@ var Paths = {
   SCSS_TOOLKIT_SOURCES: './assets/scss/black-dashboard.scss',
   SCSS: './assets/scss/**/**',
   HTML: './index.html',
-  COMPONENTS: './assets/components/'
+  COMPONENTS: './assets/components/',
+  CDN: 'https://cdn.jsdelivr.net/gh/mucic/tmehack@main/assets/'
 };
 
 // Compile SCSS
@@ -27,26 +29,28 @@ gulp.task('compile-scss', function () {
     .pipe(gulp.dest(Paths.CSS));
 });
 
-// Build HTML with partials (production)
+// Build HTML with partials (production, CDN paths)
 gulp.task('html', function () {
   return gulp.src(Paths.HTML)
     .pipe(fileInclude({
       prefix: '@@',
       basepath: Paths.COMPONENTS
-    }).on('error', function (err) { // ✅ prevent crash
+    }).on('error', function (err) {
       console.error('File include error:', err.message);
       this.emit('end');
     }))
+    // ✅ replace local asset paths with CDN
+    .pipe(replace(/\.\.\/assets\//g, Paths.CDN))
     .pipe(gulp.dest(Paths.DIST));
 });
 
-// Build HTML with partials (dev version into index_dev.html)
+// Build HTML with partials (dev version into index_dev.html, keep local assets)
 gulp.task('html-dev', function () {
   return gulp.src(Paths.HTML)
     .pipe(fileInclude({
       prefix: '@@',
       basepath: Paths.COMPONENTS
-    }).on('error', function (err) { // ✅ prevent crash
+    }).on('error', function (err) {
       console.error('File include error:', err.message);
       this.emit('end');
     }))
